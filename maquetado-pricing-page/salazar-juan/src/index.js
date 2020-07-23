335d8806ff646a0e2e34f0d9af1b12fd84d07974
matea <ryferreira1@outlook.es>
2020-07-23 17:42:34 -0300

    poniendo a prueba y creando componentes

--- maquetado-pricing-page/salazar-juan/src/index.js
+++ maquetado-pricing-page/salazar-juan/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Card from './Components/Card';
+
 
 ReactDOM.render(
-  <App />,
+  <Card />,
   document.getElementById('root')
 );
