f8fffaf509fae7490426e26c964dbc3077afea97
matea <ryferreira1@outlook.es>
2020-07-24 17:36:39 -0300

    agregando Colum a Componetes y Style

--- maquetado-pricing-page/salazar-juan/src/index.js
+++ maquetado-pricing-page/salazar-juan/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import Card from './Components/Card';
+import Colum from './Components/Colum'
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 ReactDOM.render(
-  <Card />,
+  <Card/>,
   document.getElementById('root')
 );
+
